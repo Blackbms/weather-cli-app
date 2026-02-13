267bc58fefb0364a9708b2cbb041ff2ea89cfda5
Matt Blackburn (Groot) <matt@theblackburns.us>
2026-02-13 12:08:48 -0500

    Fix linter errors

--- src/main.py
+++ src/main.py
@@ -8,9 +8,9 @@ from utils.weather_api import WeatherAPI
 
 # Explicitly set the template folder
 app = Flask(
-    __name__, 
+    __name__,
     template_folder=os.path.join(os.path.dirname(__file__), "../templates"),
-    static_folder=os.path.join(os.path.dirname(__file__), "../static")
+    static_folder=os.path.join(os.path.dirname(__file__), "../static"),
 )
 
 # Get API key from environment variable
